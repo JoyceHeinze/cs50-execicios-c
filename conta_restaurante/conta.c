a054f212c7ff981fc8800d2d14e9ed8431911f74
Joyce Heinze <joyceheinze@ufrrj.br>
2023-12-02 12:41:24 -0300

    Exercício: Dividir conta de restaurante - Refatorado

--- conta_restaurante/conta.c
+++ conta_restaurante/conta.c
@@ -3,45 +3,47 @@
 #include <stdio.h>
 #include <locale.h>
 
-// Protótipo
-void receberValores(void);
-void calcularConta(float valorConta, float taxaServico, float imposto);
+// Protótipos
+void receberValores(float *valorConta, float *taxaServico, float *imposto);
+void calcularConta(float valorConta, float taxaServico, float imposto, float *valorTotalConta, float *valorContaDividida);
 void exibirValorConta(float valorTotalConta, float valorContaDividida);
 
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-    receberValores();
-}
 
-// Valor da conta, taxa de serviço e imposto
-void receberValores(void)
-{
     float valorConta;
     float taxaServico;
     float imposto;
+    float valorTotalConta;
+    float valorContaDividida;
 
-    printf("\nPor favor, informe: \n\n1 - valor da conta em R$ (Exemplo: 000,00): ");
-    scanf("%f", &valorConta);
-    printf("\n2 - taxa de serviço em %c (Exemplo: 0,0): ", 37);
-    scanf("%f", &taxaServico);
-    printf("\n3 - imposto em %c (Exemplo: 0,0): ", 37);
-    scanf("%f", &imposto);
-
-    calcularConta(valorConta, taxaServico, imposto);
-}
-
-void calcularConta(float valorConta, float taxaServico, float imposto)
-{
-    float porcentagem = taxaServico + imposto;
-    float valorPorcentagem = valorConta * (porcentagem / 100);
-    float valorTotalConta = valorConta + valorPorcentagem;
-    float valorContaDividida = valorTotalConta / 2;
-
+    receberValores(&valorConta, &taxaServico, &imposto);
+    calcularConta(valorConta, taxaServico, imposto, &valorTotalConta, &valorContaDividida);
     exibirValorConta(valorTotalConta, valorContaDividida);
 }
 
-// Exibir no terminal valor total da conta e valor dividido por 2 pessoas.
+// Receber o valor da conta, taxa de serviço e imposto
+void receberValores(float *valorConta, float *taxaServico, float *imposto)
+{
+    printf("\nPor favor, informe: \n\n1 - valor da conta em R$ (Exemplo: 000,00): ");
+    scanf("%f", &*valorConta);
+    printf("\n2 - taxa de serviço em %c (Exemplo: 0,0): ", 37);
+    scanf("%f", &*taxaServico);
+    printf("\n3 - imposto em %c (Exemplo: 0,0): ", 37);
+    scanf("%f", &*imposto);
+}
+
+// Calcular o valor da conta com taxa de serviço e imposto
+void calcularConta(float valorConta, float taxaServico, float imposto, float *valorTotalConta, float *valorContaDividida)
+{
+    float porcentagem = taxaServico + imposto;
+    float valorPorcentagem = valorConta * (porcentagem / 100);
+    *valorTotalConta = valorConta + valorPorcentagem;
+    *valorContaDividida = *valorTotalConta / 2;
+}
+
+// Exibir no terminal valor total da conta e valor dividido por 2 pessoas
 void exibirValorConta(float valorTotalConta, float valorContaDividida)
 {
     printf("\nValor total da conta com taxa e imposto: R$%.2f \n", valorTotalConta);
